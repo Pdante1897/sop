1fcb3bd7e68075c07e99a4d34c3c77db4e4eae09
Bryan Paez <bryanpaez.125@gmail.com>
2023-10-01 19:09:11 -0600

    fix

--- proyecto1/frontend/src/App.js
+++ proyecto1/frontend/src/App.js
@@ -38,17 +38,23 @@ function App() {
   };
   
   return (
-    <div>
-      <h1>Selecciona una Máquina</h1>
-      <select value={maquinaSeleccionada} onChange={handleSeleccionMaquina}>
-        {opcionesMaquinas.map((maquina) => (
-          <option key={maquina.numero} value={maquina.numero}>
-            {maquina.nombre}
-          </option>
-        ))}
-      </select>
-      <p>Has seleccionado: Maquina {maquinaSeleccionada}</p>
-    </div>
+
+    
+    <div className="container">
+      <h1 className='titulo-h1'>Proyecto 1 Sopes 1</h1>
+      <div>
+  <h1>Selecciona una Máquina</h1>
+  <select value={maquinaSeleccionada} onChange={handleSeleccionMaquina}>
+    {opcionesMaquinas.map((maquina) => (
+      <option key={maquina.numero} value={maquina.numero}>
+        {maquina.nombre}
+      </option>
+    ))}
+  </select>
+  <p>Has seleccionado: Maquina {maquinaSeleccionada}</p>
+</div>
+
+      
       <div className='container-table-1'>
         <Table/>
       </div>
